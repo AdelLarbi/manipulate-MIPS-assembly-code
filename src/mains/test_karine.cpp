0a1d2bcc538d166cb118fead5d96df799c685edb
AdelLarbi <larbi.adel@yahoo.fr>
2016-04-27 19:08:02 +0200

    test_Karine.cpp modified

--- src/mains/test_karine.cpp
+++ src/mains/test_karine.cpp
@@ -7,24 +7,33 @@
 #include <Cfg.h>
 
 /* 
-parsing d'un fichier assembleur 
-affichage du programme
-calcul des fonction, affichage du nombre de fonction
-EXEMPLE DE TEST des méthodes que vous devez coder
-*/
+ * EXEMPLE DE TEST des méthodes que vous devez coder
+ */
 
-int main(int argc, char ** argv){
-  if (argc < 2){
+int main(int argc, char ** argv) {
+  
+  if (argc < 2) {
     cerr << "pas de fichier assembleur en paramètre, pb !" << endl;
     return -1;
   }
-  Program p2(argv[1]) ;
-  p2.display() ;
+  
+  // parsing d'un fichier assembleur 
+  Program p2(argv[1]);
+
+  // affichage du programme
+  p2.display();
+
+  // calcul des fonction
   p2.comput_function();
+
+  // affichage du nombre de fonction
   cout << "dans le fichier " << argv[1]  << " nb function : " << p2.nbr_func() << endl;
-  for (int i = 0; i < p2.nbr_func() ; i++){
-    cout << " ----------------- FONCTION "<< i<< " --------------------" << endl;
-     p2.get_function(i)->display();
+
+  for (int i = 0; i < p2.nbr_func() ; i++) {
+    cout << " ----------------- FONCTION " << i<< " --------------------" << endl;
+    
+    // affichage du fonction
+    p2.get_function(i)->display();
   }
     
   return 0;
